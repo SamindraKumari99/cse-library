12636922dc9b0aae51eb32a34f9bc4982fa5a383
NethumL <nethumpinsara@gmail.com>
2021-09-23 11:15:45 +0530

    Implement factorial function

--- src/main/java/it/uom/cse/MathOperation.java
+++ src/main/java/it/uom/cse/MathOperation.java
@@ -15,4 +15,11 @@ public class MathOperation {
         return operand1 - operand2;
     }
 
+    public static int factorial(int n) {
+        int result = 1;
+        for (int i = 1; i < n; i++) {
+            result *= i;
+        }
+        return result;
+    }
 }
