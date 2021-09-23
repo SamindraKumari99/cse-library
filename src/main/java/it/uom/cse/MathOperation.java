3a35f1304edbeef3ac53dc3e399000cb21f2b4dc
Sathira443 <liyanapathiranasathira@gmail.com>
2021-09-23 12:19:50 +0530

    team_15_role_D_sumOfPrimes

--- src/main/java/it/uom/cse/MathOperation.java
+++ src/main/java/it/uom/cse/MathOperation.java
@@ -5,13 +5,11 @@ import java.util.Arrays;
 
 public class MathOperation {
 
-    public static double add(double... operands)
-    {
+    public static double add(double... operands) {
         return Arrays.stream(operands).reduce(0d, Double::sum);
     }
 
-    public static double subtract(double operand1, double operand2)
-    {
+    public static double subtract(double operand1, double operand2) {
         return operand1 - operand2;
     }
 
@@ -22,13 +20,38 @@ public class MathOperation {
 
         return fib(n - 1) + fib(n - 2);
     }
-    public static int sumOfIntegers(int n){
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum=sum+i;
+
+    public static int sumOfIntegers(int n) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum = sum + i;
         }
         return sum;
 
     }
+
+    public static int sumOfPrimes(int n){
+        int sum = 0;
+
+        for(int num = 2; num <= n; num++) {
+
+            int i;
+
+            for(i = 2; i <= (num / 2); i++) {
+
+                if(num % i == 0) {
+                    i = num;
+                    break;
+                }
+            }
+
+            if(i != num) {
+                sum += num;
+            }
+        }
+
+        return sum;
+    }
+
 
 }
